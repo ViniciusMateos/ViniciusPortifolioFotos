59f6c0c8f098be6277a59e0742751750c2a0e2a6
Vinicius Mateos <vinimateos.s@gmail.com>
2024-03-08 03:17:41 -0300

    Menu mobile implementado em todas as telas

--- sobre/scripts.js
+++ sobre/scripts.js
@@ -1,11 +1,3 @@
-// MAGNIFIC POPUP
-$(document).ready(function() {
-    $('.portifolio').magnificPopup({type:'image'});
-  });
-
-// MENU MOBILE
-
-// "let" para declarar uma variável
 let btnMenu =  document.getElementById('btn-abrir-menu')
 let menu = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay-menu')
